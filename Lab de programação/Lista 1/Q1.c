18b2bf7acc2111a4fed7507df04e3d70fc34c3d8
bruno20614 <120686645+bruno20614@users.noreply.github.com>
2023-08-17 16:00:34 -0300

    Q1.c

---
+++ Lab de programação/Lista 1/Q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+  int x;
+  x = 7 + 3*6/2-1; 
+  printf("%d\n",x);
+  x = 2%2 + 2*2-2/2;
+  printf("%d\n",x);
+  x = (3*9*(3+(9*3/(3))));
+  printf("%d\n",x);
+  return 0;
+}
