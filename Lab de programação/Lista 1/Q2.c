fef3f319455d432bf83821ac7426b39f2ca3f5f5
bruno20614 <120686645+bruno20614@users.noreply.github.com>
2023-08-17 16:03:04 -0300

    Q2.c

---
+++ Lab de programação/Lista 1/Q2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  int x;
+  printf("Digite um número inteiro:\n");
+  scanf("%d",&x);
+  printf("O número em hexadeciaml é:%x \n",x);
+  printf("O número em octal é:%o",x);
+  return 0;
+}
