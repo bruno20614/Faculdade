c577e18007ba9df8b649e26e68cadc7551330236
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-08-24 16:24:08 -0300

    Create Q22.c

---
+++ Lab de programação/Lista 1/Q22.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{   
+    //O valor inteiro que representa A
+    printf("%d\n", 'A');
+    //O valor inteiro que representa B
+    printf("%d\n", 'C');
+    //O valor inteiro que representa C
+    printf("%d\n", 'a');
+    //O valor inteiro que representa a
+    printf("%d\n", 'b');
+    //O valor inteiro que representa b
+    printf("%d\n", 'c');
+    //O valor inteiro que representa c
+    printf("%d\n", '0');
+    //O valor inteiro que representa 0
+    printf("%d\n", '1');
+    //O valor inteiro que representa 1
+    printf("%d\n", '2');
+    //O valor inteiro que representa 2
+    printf("%d\n", '$');
+    //O valor inteiro que representa $
+    printf("%d\n", '*');
+    //O valor inteiro que representa +
+    printf("%d\n", '+');
+    //O valor inteiro que representa /
+    printf("%d\n", '/');
+    return 0;
+}
