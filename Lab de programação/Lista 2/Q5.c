177bd16e59bb246d5dc9f5246e05372320e17b33
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-08-29 20:15:42 -0300

    Create Q5.c

---
+++ Lab de programação/Lista 2/Q5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  int main(){
+   int maior, num,cont;
+    while(1){
+      puts ("Digite um número");
+    scanf("%d",&num);
+    if (cont=0){
+      maior=num;
+    }
+     if (maior<num){
+       maior=num;
+     } 
+       printf("O maior número é %d \n",maior);
+          cont++;     
+            }        
+ return 0;
+            }
