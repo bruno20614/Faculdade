e199ba978e37ae18c3d88e2f48ac382cc3f0ea08
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-08-30 15:38:14 -0300

    Update Q6.c

--- Lab de programação/Lista 2/Q6.c
+++ Lab de programação/Lista 2/Q6.c
@@ -47,23 +47,3 @@ int main() {
 }
 return 0;
 }
-
-
-
-#include <stdio.h>
-
-int main() {
-    int num1,num2,resultado;
-  puts("Digite um valor");
-  scanf("%d",&num1);
-  puts("Digite um valor");
-  scanf("%d",&num2);
-    while (num2==0){
-      puts("Digite o segundo número");
-      scanf("%d",&num2);
-      break;
-    }
-    resultado=num1/num2;
-    printf("resultado %d \n",resultado);
-return 0;
-           }
