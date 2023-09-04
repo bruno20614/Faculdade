a37947db92d5d646663afa745ea47b177efd9387
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-09-04 14:40:18 -0300

    Create Q18.c

---
+++ Lab de programação/Lista 2/Q18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define  TAM 64
+int main(void) {
+  
+  long double somatorio=1,inicio=1;
+  for(int i=2;i<=TAM;i++){
+
+    inicio = inicio*2;
+      somatorio+=inicio;
+  }
+     printf("o resultado é %Lf \n",somatorio);
+  return 0;
+}
