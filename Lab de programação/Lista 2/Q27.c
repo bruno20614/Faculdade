c8b9f9e679ccc5a2e9bf4beed7d71b08c39b9c30
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-09-05 20:12:18 -0300

    Update Q27.c

--- Lab de programação/Lista 2/Q27.c
+++ Lab de programação/Lista 2/Q27.c
@@ -6,16 +6,17 @@ int main( ){
     puts("Digite um número");
     scanf("%d",&num);
 
+    if (num < 0 && num % 2 == 0){
+
+         break;
+    }
     if (cont==0|| num>maior){
        maior=num;
     }
     if (cont==0|| num<menor){
        menor=num;
     }
-    if (num < 0 && num % 2 == 0){
-
-         break;
-    }
+    
     cont++;
          }
            printf("O resultado é %d \n",(maior*menor));
