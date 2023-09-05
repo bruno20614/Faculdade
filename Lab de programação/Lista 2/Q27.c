94c565e531e930c9d533a5a0314b3ce68a6d6e0f
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-09-05 20:09:21 -0300

    Create Q27.c

---
+++ Lab de programação/Lista 2/Q27.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main( ){
+  int maior,menor,num,cont=0;
+  while(1){
+    puts("Digite um número");
+    scanf("%d",&num);
+
+    if (cont==0|| num>maior){
+       maior=num;
+    }
+    if (cont==0|| num<menor){
+       menor=num;
+    }
+    if (num < 0 && num % 2 == 0){
+
+         break;
+    }
+    cont++;
+         }
+           printf("O resultado é %d \n",(maior*menor));
+  return 0;
+  
+         } 
