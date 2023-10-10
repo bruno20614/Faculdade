5c8a0003062c386e5c7927e9d07a7958b9f71bad
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-10-09 22:15:51 -0300

    Update Q1.c

--- Lab de programação/Lista 3/Q1.c
+++ Lab de programação/Lista 3/Q1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 15
+int main(){
+    int a, maior=0, menor=0;
+    float Notas[TAM];
 
+    printf("digite 15 notas");
+    printf("\n");
+    for (a=0;a<TAM;a++){
+      printf("nota %d : ", a+1);
+      scanf("%f", &Notas[a]);
+      if(a==0){maior=Notas[a];menor=Notas[a];}
+      if(Notas[a]>maior){
+        maior=Notas[a];
+      }
+      else{
+        if(Notas[a]<menor){
+            menor=Notas[a];
+        }
+      }
+    }
+    printf("\no maior valor é %d\n", maior);
+    printf("\ne o menor valor é %d\n\n", menor);
+    return 0;
+}
