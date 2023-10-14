6dfd4b76c87894ef025404640914b33a08def469
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-10-14 11:07:34 -0300

    Update Q2.c

--- Lab de programação/Lista 4/Q2.c
+++ Lab de programação/Lista 4/Q2.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void trocarValores(int *a, int *b) {
-    int c = *a;
-    *a = *b;
-    *b = c;
-}
-
-int main() {
-    int num1, num2;
-
-    printf("Digite o primeiro número: ");
-    scanf("%d", &num1);
-
-    printf("Digite o segundo número: ");
-    scanf("%d", &num2);
-
-    printf("Valores antes da troca: num1 = %d, num2 = %d\n", num1, num2);
-
-    trocarValores (&num1, &num2);
-
-    printf("Valores depois da troca: num1 = %d, num2 = %d\n", num1, num2);
-
-    return 0;
+int main(){
+  int a;int b;  int *p1; int *p2; int **p3;
+  p1 = &a;
+  p2 = &b;
+  puts("Digite o primeiro valor:");
+  scanf("%d",p1);
+  puts("Digite o segundo valor:");
+  scanf("%d",p2);
+  **p3 = *p1;
+  *p1 = *p2;
+  *p2 = **p3; 
+  printf("O primeiro valor agora é:%d\n",*p1);
+  printf("O endereço de memória do primeiro valor é: %p\n",p1);
+  printf("O segundo valor agora é:%d\n",*p2);
+  printf("O endereço de memória do segundo valor é: %p\n",p2);
+  
+  return 0;
 }
