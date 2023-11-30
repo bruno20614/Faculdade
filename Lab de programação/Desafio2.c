7809af884966f4d76ad383b88db357b5dc863b1b
bruno206 <120686645+bruno20614@users.noreply.github.com>
2023-11-30 19:40:47 -0300

    Update Desafio2.c

--- Lab de programação/Desafio2.c
+++ Lab de programação/Desafio2.c
@@ -1,115 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct BlocoLivre {
+    int tamanho;
+    struct BlocoLivre* proximo;
+} BlocoLivre;
 
-// Estrutura para representar um bloco de memória livre
-typedef struct FreeBlock {
-    int size;
-    struct FreeBlock* next;
-} FreeBlock;
-
-// Função para inicializar a lista de espaços livres com uma configuração inicial
-FreeBlock* initializeMemory() {
-    // Aqui, você pode inicializar a lista de espaços livres conforme necessário.
-    // Por exemplo, criar uma lista com alguns blocos de tamanhos diferentes.
-    FreeBlock* head = (FreeBlock*)malloc(sizeof(FreeBlock));
-    head->size = 100;  // Tamanho inicial da memória
-    head->next = NULL;
-
-    return head;
+BlocoLivre* inicializarMemoria() {
+    BlocoLivre* cabeca = malloc(sizeof(BlocoLivre));
+    cabeca->tamanho = 100;
+    cabeca->proximo = NULL;
+    return cabeca;
 }
 
-// Função para exibir a lista de espaços livres
-void displayFreeBlocks(FreeBlock* head) {
-    FreeBlock* current = head;
-
-    printf("Espaços livres: ");
-    while (current != NULL) {
-        printf("[%d] -> ", current->size);
-        current = current->next;
+void exibirBlocosLivres(BlocoLivre* cabeca) {
+    printf("Blocos livres: ");
+    while (cabeca) {
+        printf("[%d] -> ", cabeca->tamanho);
+        cabeca = cabeca->proximo;
     }
     printf("Fim\n");
 }
 
-// Algoritmo First-Fit para alocar memória
-int allocateFirstFit(FreeBlock** head, int size) {
-    FreeBlock* current = *head;
-    FreeBlock* prev = NULL;
+int alocarBloco(BlocoLivre** cabeca, int tamanho) {
+    BlocoLivre* atual = *cabeca;
+    BlocoLivre* anterior = NULL;
 
-    while (current != NULL) {
-        if (current->size >= size) {
-            // Encontrou um bloco grande o suficiente
-            if (current->size == size) {
-                // O bloco é exatamente do tamanho necessário
-                if (prev == NULL) {
-                    // Remover o bloco do início
-                    *head = current->next;
-                } else {
-                    // Remover o bloco do meio ou final
-                    prev->next = current->next;
-                }
-            } else {
-                // Dividir o bloco em dois, alocando o necessário
-                FreeBlock* newBlock = (FreeBlock*)malloc(sizeof(FreeBlock));
-                newBlock->size = size;
-                newBlock->next = NULL;
-
-                current->size -= size;
-                newBlock->next = current->next;
-                current->next = newBlock;
-            }
-
-            return 1;  // Alocado com sucesso
-        }
-
-        prev = current;
-        current = current->next;
+    while (atual && atual->tamanho < tamanho) {
+        anterior = atual;
+        atual = atual->proximo;
     }
 
-    return 0;  // Não há espaço suficiente
+    if (!atual)
+        return 0;  // Não há espaço suficiente
+
+    if (atual->tamanho == tamanho) {
+        if (!anterior)
+            *cabeca = atual->proximo;
+        else
+            anterior->proximo = atual->proximo;
+    } else {
+        BlocoLivre* novoBloco = malloc(sizeof(BlocoLivre));
+        novoBloco->tamanho = tamanho;
+        novoBloco->proximo = atual->proximo;
+
+        atual->tamanho -= tamanho;
+        atual->proximo = novoBloco;
+    }
+
+    return 1;  // Alocado com sucesso
 }
 
-// Algoritmo Best-Fit para alocar memória
-int allocateBestFit(FreeBlock** head, int size) {
-    // Implemente o algoritmo Best-Fit aqui
-
-    return 0;  // Temporário - substitua pela lógica real
-}
-
-// Função principal
 int main() {
-    FreeBlock* memory = initializeMemory();
-    int choice, size;
+    BlocoLivre* memoria = inicializarMemoria();
+    int escolha, tamanho;
 
     do {
-        displayFreeBlocks(memory);
+        exibirBlocosLivres(memoria);
         printf("Escolha o algoritmo (1 para First-Fit, 2 para Best-Fit, 0 para sair): ");
-        scanf("%d", &choice);
+        scanf("%d", &escolha);
 
-        if (choice != 0) {
+        if (escolha != 0) {
             printf("Informe o tamanho do bloco necessário: ");
-            scanf("%d", &size);
+            scanf("%d", &tamanho);
 
-            switch (choice) {
+            switch (escolha) {
                 case 1:
-                    if (!allocateFirstFit(&memory, size)) {
-                        printf("Não há espaço livre suficiente para alocar %d bytes.\n", size);
-                    }
+                    if (!alocarBloco(&memoria, tamanho))
+                        printf("Não há espaço livre suficiente para alocar %d bytes.\n", tamanho);
                     break;
                 case 2:
-                    // Implemente chamada para alocar usando Best-Fit
+                    // Adicione aqui a chamada para alocar usando Best-Fit
                     break;
                 default:
                     printf("Escolha inválida. Tente novamente.\n");
             }
         }
-    } while (choice != 0);
+    } while (escolha != 0);
 
-    // Libera a memória restante na lista de espaços livres
-    FreeBlock* current = memory;
-    while (current != NULL) {
-        FreeBlock* next = current->next;
-        free(current);
-        current = next;
+    while (memoria) {
+        BlocoLivre* proximo = memoria->proximo;
+        free(memoria);
+        memoria = proximo;
     }
 
     return 0;
