183b89ac601ad8f8294d916a834bd4ac5c7b3b25
Bruno Costa <brunocdl9@gmail.com>
2025-06-23 15:39:59 -0300

    sla

---
+++ Análise de Algoritmos/Progamação Dinâmica/q1.py
@@ -0,0 +1,31 @@
+#Tendo subproblema
+
+def Foo(A, n):
+    if n <= 4:
+        return 0 
+    
+    return Foo(A, n//2) + Foo(A, n//2 +1) + Foo(A, n//2 + 2) + A[n-1]
+
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n = 10
+
+resultado = Foo(A, n)
+print(resultado)
+
+
+#Usando memoização, por que não memorização?
+def Foo(A, n,memo = None):
+    if n <= 4:
+        return 0
+    
+    if memo is None:
+        memo = {}
+  
+    for n in memo:
+      return memo[n]
+
+    val = (Foo(A, n//2,memo) + Foo(A, n//2 + 1,memo) + Foo(A, n//2 + 2,memo) + A[n-1]) 
+
+    memo[n] = val
+    return val
+
